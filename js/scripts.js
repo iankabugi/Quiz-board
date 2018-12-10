d7ffe767b023a45de06aa420ba9f5f4580cf8210
Ian Kabugi <iank299@gmail.com>
2018-12-10 16:35:21 +0300

    amend code to function correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
-var myAnswer = $("input:radio[name=myAnswer]:checked").val();
-  return myAnswer
+$(document).ready(function() {
+  $("button#Submit").click(function() {
+    //event.preventDefault();
+    var questOne = $("input:radio[name=answerOne]:checked").val();
+    var questTwo = $("input:radio[name=answerTwo]:checked").val();
+    var questThree = $("input:radio[name=answerThree]:checked").val();
+    var questFour = $("input:radio[name=answerFour]:checked").val();
+    var questFive = $("input:radio[name=answerFive]:checked").val();
