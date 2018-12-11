5008e74da2b211e87a8cd843ac1950c81c4f19b7
Ian Kabugi <iank299@gmail.com>
2018-12-11 10:05:32 +0300

    change class from jumbotron to container

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-  $("button#Submit").click(function() {
-    //event.preventDefault();
-    var questOne = $("input:radio[name=answerOne]:checked").val();
-    var questTwo = $("input:radio[name=answerTwo]:checked").val();
-    var questThree = $("input:radio[name=answerThree]:checked").val();
-    var questFour = $("input:radio[name=answerFour]:checked").val();
-    var questFive = $("input:radio[name=answerFive]:checked").val();
+  $("#submit").click(function() {
+
+    var attemptOne = $("input:radio[name=answerOne]:checked").val();
+    var attemptTwo = $("input:radio[name=answerTwo]:checked").val();
+    var attemptThree = $("input:radio[name=answerThree]:checked").val();
+    var attemptFour = $("input:radio[name=answerFour]:checked").val();
+    var attemptFive = $("input:radio[name=answerFive]:checked").val();
+
+var totalScore =parseInt(attemptOne)+ parseInt(attemptTwo) + parseInt(attemptThree) + parseInt(attemptFour) + parseInt(attemptFive)
+var percentageScore= (totalScore/100)*100
+var percentageScore=percentageScore.toString() + "%"
+$("#submit").text(percentageScore);
+$(".container").hide(function(event){
+    alert(percentageScore);
+  });
+  });
+});
